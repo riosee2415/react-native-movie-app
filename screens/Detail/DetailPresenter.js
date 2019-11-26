be1bb3fc95813e2dcfa0da913a801f659e6bca6f
4leaf.ysh <riosee2415@gmail.com>
2019-11-26 11:31:15 +0900

    send data to detail

--- screens/Detail/DetailPresenter.js
+++ screens/Detail/DetailPresenter.js
@@ -1,6 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import { BG_COLOR, TINT_COLOR } from "../../constants/Colors";
+import Layout from "../../constants/Layout";
+import MoviePoster from "../../components/MoviePoster";
+import makePhotoUrl from "../../utils/makePhotoUrl";
+
+const Container = styled.ScrollView`
+  background-color: ${BG_COLOR};
+  flex: 1;
+`;
+
+const Header = styled.View`
+  position: relative;
+`;
+
+const BgImage = styled.Image`
+  width: ${Layout.width};
+  height: ${Layout.height / 3.5};
+  opacity: 0.3;
+  position: absolute;
+  top: 0;
+`;
+
+const Content = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: flex-end;
+  padding-horizontal: 30px;
+  height: ${Layout.height / 3.5};
+`;
+
+const Column = styled.View`
+  margin-left: 30px;
+`;
+
+const Title = styled.Text`
+  color: ${TINT_COLOR};
+  font-size: 14px;
+  font-weight: 600;
+`;
 
 const DetailPresenter = ({
   id,
@@ -9,8 +48,29 @@ const DetailPresenter = ({
   title,
   voteAvg,
   overview
-}) => null;
+}) => {
+  return (
+    <Container>
+      <Header>
+        <BgImage source={{ uri: makePhotoUrl(backgroundPhoto) }} />
+        <Content>
+          <MoviePoster path={posterPhoto} />
+          <Column>
+            <Title>{title}</Title>
+          </Column>
+        </Content>
+      </Header>
+    </Container>
+  );
+};
 
-DetailPresenter.prototype = {};
+DetailPresenter.prototype = {
+  id: PropTypes.number.isRequired,
+  posterPhoto: PropTypes.string.isRequired,
+  backgroundPhoto: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  voteAvg: PropTypes.number,
+  overview: PropTypes.string
+};
 
 export default DetailPresenter;
